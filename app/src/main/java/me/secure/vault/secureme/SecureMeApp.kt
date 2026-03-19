e07adf270cf417b4c18a2c8e2d4f7e9715d6a2d6
muhammadibrahim <mik237@gmail.com>
2026-03-19 11:55:27 +0500

    Stage 17: v2. removed reCaptcha and play integrity check

--- app/src/main/java/me/secure/vault/secureme/SecureMeApp.kt
+++ app/src/main/java/me/secure/vault/secureme/SecureMeApp.kt
@@ -2,9 +2,6 @@ package me.secure.vault.secureme
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
-import com.google.firebase.appcheck.FirebaseAppCheck
 import dagger.hilt.android.HiltAndroidApp
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.Security
@@ -13,26 +10,12 @@ import java.security.Security
 class SecureMeApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
         setupBouncyCastle()
-        setupFirebaseAppCheck()
     }
 
     private fun setupBouncyCastle() {
         Security.removeProvider("BC")
         Security.addProvider(BouncyCastleProvider())
-    }
-
-    private fun setupFirebaseAppCheck() {
-        FirebaseApp.initializeApp(this)
-        val firebaseAppCheck = FirebaseAppCheck.getInstance()
-        if (BuildConfig.DEBUG) {
-            firebaseAppCheck.installAppCheckProviderFactory(
-                DebugAppCheckProviderFactory.getInstance()
-            )
-        } else {
-            firebaseAppCheck.installAppCheckProviderFactory(
-                PlayIntegrityAppCheckProviderFactory.getInstance()
-            )
-        }
     }
 }
